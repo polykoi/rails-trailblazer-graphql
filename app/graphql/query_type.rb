9bb6ba55b9395ba8dc30dbd6d5f89f574a3aa821
polykoi <lambwithastick@gmail.com>
2019-08-09 18:07:57 +0300

    ???

--- app/graphql/query_type.rb
+++ app/graphql/query_type.rb
@@ -1,18 +1,40 @@
 class QueryType < GraphQL::Schema::Object
-  field :users, resolver: Users::Resolver::Index
-  field :user, resolver: Users::Resolver::Show
+  # field :users, resolver: Users::Resolver::Index
+  # field :user, resolver: Users::Resolver::Show
   field :current_user, resolver: Users::Resolver::CurrentUser
-  # field :boards, [BoardType], null: true
-  # field :board, BoardType, null: true do
-  #   argument :id, ID, 'Id', required: true
-  # end
-
-
-  # def boards
-  #   Board.all
-  # end
+  field :board, BoardType, null: true do
+    argument :id, ID, 'Id', required: true
+  end
+  field :card
 
   # def board(id:)
   #   Board.find_by(id: id)
   # end
 end
+
+# type user {
+#   [Boards]
+#   [Cards]
+# }
+
+# type users {
+#   [Boards]
+#   [Cards]
+# }
+
+# type current_user {
+#   [Boards]
+#   [Cards]
+# }
+
+# type board {
+#   [Statuses]
+#   [Cards]
+#   [Users]
+# }
+
+# type card {
+#   Board
+#   Status
+#   User
+# }
